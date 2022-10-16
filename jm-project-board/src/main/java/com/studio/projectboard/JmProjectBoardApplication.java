522ca237ab0c58f21b92a4dd031872b7871346e5
jjmin9797 <jjmin9797@naver.com>
2022-10-16 14:30:45 +0900

    project start

---
+++ jm-project-board/src/main/java/com/studio/projectboard/JmProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.studio.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmProjectBoardApplication.class, args);
+    }
+
+}
---
+++ jm-project-board/src/test/java/com/studio/projectboard/JmProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studio.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
