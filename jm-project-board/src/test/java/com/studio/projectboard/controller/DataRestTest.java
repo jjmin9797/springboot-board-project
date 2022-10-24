0c8e893c48a9ba8354533ccbd747a70cab2e0dea
jjmin9797 <jjmin9797@naver.com>
2022-10-24 22:08:01 +0900

    #5.1 - QueryDSL 디펜던시 추가 및 충돌 방지

--- jm-project-board/src/test/java/com/studio/projectboard/controller/DataRestTest.java
+++ jm-project-board/src/test/java/com/studio/projectboard/controller/DataRestTest.java
@@ -1,6 +1,7 @@
 package com.studio.projectboard.controller;
 
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외시킴")
 @DisplayName("api test")
 @AutoConfigureMockMvc
 @SpringBootTest
