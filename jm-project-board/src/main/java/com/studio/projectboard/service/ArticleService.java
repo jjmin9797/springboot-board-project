dc1c7b36a79537ee7c20a4c04e09337b28c0994a
jjmin9797 <jjmin9797@naver.com>
2022-11-30 02:00:58 +0900

    feat: 게시판 index 페이지 기능 구현

--- jm-project-board/src/main/java/com/studio/projectboard/service/ArticleService.java
+++ jm-project-board/src/main/java/com/studio/projectboard/service/ArticleService.java
@@ -1,18 +1,23 @@
 package com.studio.projectboard.service;
 
 import com.studio.projectboard.domain.Article;
+import com.studio.projectboard.domain.UserAccount;
 import com.studio.projectboard.domain.type.SearchType;
 import com.studio.projectboard.dto.ArticleDto;
 import com.studio.projectboard.dto.ArticleWithCommentsDto;
+import com.studio.projectboard.dto.UserAccountDto;
 import com.studio.projectboard.repository.ArticleRepository;
+import com.studio.projectboard.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.asm.Advice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
 
 //필수 생성자 자동 생성
 @RequiredArgsConstructor
@@ -22,6 +27,7 @@ import javax.persistence.EntityNotFoundException;
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+    private final UserAccountRepository userAccountRepository;
 
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
@@ -64,4 +70,6 @@ public class ArticleService {
     public void deleteArticle(ArticleDto dto) {
         articleRepository.deleteById(dto.id());
     }
+
+
 }
--- jm-project-board/src/test/java/com/studio/projectboard/controller/ArticleControllerTest.java
+++ jm-project-board/src/test/java/com/studio/projectboard/controller/ArticleControllerTest.java
@@ -1,12 +1,25 @@
 package com.studio.projectboard.controller;
 
+import com.studio.projectboard.config.SecurityConfig;
+import com.studio.projectboard.dto.ArticleWithCommentsDto;
+import com.studio.projectboard.dto.UserAccountDto;
+import com.studio.projectboard.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -14,22 +27,86 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @DisplayName("View 컨트롤러 - 게시글")
 @WebMvcTest(ArticleController.class)
+@Import(SecurityConfig.class)
 class ArticleControllerTest {
 
+
     private final MockMvc mvc;
+
+    @MockBean
+    private ArticleService articleService;
 
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
-    @Disabled("개발중")
+
+
     @DisplayName("[view] [Get] 게시글 리스트")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenRetrunArticlesView() throws Exception {
+        //given
+        BDDMockito.given(articleService.searchArticles(ArgumentMatchers.eq(null), ArgumentMatchers.eq(null), BDDMockito.any(Pageable.class)))
+                .willReturn(Page.empty());
+
+        //when
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
-                .andExpect(model().attributeExists("articles"));
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("searchTypes"));
+
+        //& then
+        BDDMockito.then(articleService)
+                .should()
+                .searchArticles(ArgumentMatchers.eq(null), ArgumentMatchers.eq(null), BDDMockito.any(Pageable.class));
     }
 
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상호출")
+    @Test
+    void  givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception{
+        //given
+        Long articleId = 1L;
+        BDDMockito.given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
+
+        //when & then
+        mvc.perform(get("/articles/"+articleId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
+        BDDMockito.then(articleService).should().getArticle(articleId);
+
+    }
+
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#java",
+                LocalDateTime.now(),
+                "jm",
+                LocalDateTime.now(),
+                "jm"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+                1L,
+                "uno",
+                "password",
+                "uno@mail.com",
+                "Uno",
+                "This is memo",
+                LocalDateTime.now(),
+                "uno",
+                LocalDateTime.now(),
+                "uno"
+        );
+    }
 }
