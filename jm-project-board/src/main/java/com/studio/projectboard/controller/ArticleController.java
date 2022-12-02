a55494f81eaf6dc6322e7afc56e12bc2ff1b7a90
jjmin9797 <jjmin9797@naver.com>
2022-12-02 22:23:26 +0900

    feat: 게시판 검색 구현

--- jm-project-board/src/main/java/com/studio/projectboard/controller/ArticleController.java
+++ jm-project-board/src/main/java/com/studio/projectboard/controller/ArticleController.java
@@ -1,10 +1,13 @@
 package com.studio.projectboard.controller;
 
+import com.studio.projectboard.domain.Article;
 import com.studio.projectboard.domain.type.SearchType;
 import com.studio.projectboard.dto.response.ArticleResponse;
 import com.studio.projectboard.dto.response.ArticleWithCommentsResponse;
 import com.studio.projectboard.service.ArticleService;
+import com.studio.projectboard.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @RequestMapping("/articles")
 @Controller
 @RequiredArgsConstructor
@@ -22,6 +27,7 @@ public class ArticleController {
 
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -30,7 +36,11 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginaionBarNumbers(pageable.getPageNumber(),articles.getTotalPages());
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+        map.addAttribute("searchTypes", SearchType.values());
         return "articles/index";
     }
 
@@ -45,9 +55,5 @@ public class ArticleController {
         return "articles/detail";
     }
 
-    @GetMapping("/detail")
-    public String addArticle() {
-        return "articles/detail";
-    }
 
 }
